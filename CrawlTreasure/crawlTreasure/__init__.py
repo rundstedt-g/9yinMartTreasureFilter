f7d8b5b75fc6fd6a8381751e14607d2db80daf21
Wilson <Wilson>
2022-03-20 20:05:12 +0800

    feat: 文件结构调整

--- CrawlTreasure/crawlTreasure/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import sys
-import os
-curPath = os.path.abspath(os.path.dirname(__file__))
-rootPath = os.path.split(curPath)[0]
-sys.path.append(rootPath)
-from scrapy import cmdline
-cmdline.execute("scrapy crawl crawlTreasure -a serverId=186100101".split())
